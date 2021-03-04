b915e31ecab92effea5ae21c7322026efef985d5
Valentin <ciolentin@yahoo.com>
2021-03-04 19:20:15 +0200

    init

---
+++ main.js
@@ -0,0 +1,123 @@
+// add students to database with name, age and grades
+// make an average of the grades inputed
+// delete the last student in the list on button click
+
+var gpaTable = [];
+
+
+function generateTable() {
+
+    var name = document.getElementById('name').value;
+    var age = document.getElementById('age').value;
+    var math = document.getElementById('math').value;
+    var english = document.getElementById('english').value;
+    var history = document.getElementById('history').value;
+    var coding = document.getElementById('coding').value; 
+    var tBody = document.getElementsByClassName("tbody")[0];
+    var frm = document.getElementById("submit-form")[0];
+
+    if (name == "" || age == "" || math == "" || english == "" || history == "" || coding == "") {
+        console.log("neev Values");
+    } else {
+        for (var i = 0; i < 1; i++) {
+            var row = document.createElement("tr");
+
+            for (var j = 0; j < 1; j++) {
+                var cell = document.createElement("td");
+                var cellText = document.createTextNode(name);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            for (var j = 0; j < 1; j++) {
+                var cell = document.createElement("td");
+                var cellText = document.createTextNode(age);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            for (var j = 0; j < 1; j++) {
+                var cell = document.createElement("td");
+                var cellText = document.createTextNode(math);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            for (var j = 0; j < 1; j++) {
+                var cell = document.createElement("td");
+                var cellText = document.createTextNode(english);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            for (var j = 0; j < 1; j++) {
+                var cell = document.createElement("td");
+                var cellText = document.createTextNode(history);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            for (var j = 0; j < 1; j++) {
+                var cell = document.createElement("td");
+                var cellText = document.createTextNode(coding);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            for (var j = 0; j < 1; j++) {
+                var cell = document.createElement("td");
+                var gpa = (Number(math) + Number(english) + Number(history) + Number(coding)) / 4;
+                
+                var cellText = document.createTextNode(gpa);
+        
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+
+            tBody.appendChild(row);
+        }
+
+        gpaTable.push(gpa);
+        gpaTable.sort(function(a, b) {
+            return a - b;
+        });
+
+        console.log(gpaTable);
+    }
+    
+    
+
+    document.getElementById('submit-form').reset();
+}
+
+
+function orderRows() {
+    var table, i, x, y;
+    table = document.getElementById("table");
+    var switching = true;
+        while(switching) {
+            switching = false;
+            var rows = table.rows;
+
+            for(i = 1; i < (rows.length - 1); i++) {
+                var shouldSwitch = false;
+                x = rows[i].getElementsByTagName("TD")[6].innerHTML;
+                y = rows[i + 1].getElementsByTagName("TD")[6].innerHTML;
+                if (x > y) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+              }
+        }
+}
+
+
+
+function deleteRow() {
+    var table = document.getElementById("table");
+    var rowCount = table.rows.length;
+    if(rowCount == 1) {
+        alert("Nothing to delete")
+    } else {
+        table.deleteRow(rowCount - 1);
+    }
+    
+}
---
+++ sintax.js
@@ -0,0 +1,45 @@
+var x;
+let y;
+const z;
+
+function name(param1, param2) {
+    //doStuff
+}
+
+var someFunction = function() {
+    //do more stuff
+}
+
+var shortFunction = (a) => {/*do stuff */ }
+
+var array = [1, 2, 3];
+
+var object = {
+    key1: "value",
+    key2: "value2",
+
+}
+
+for(let i = 0; i < array.length; i++) {
+    //dsdsds
+}
+
+array.forEach(element => {
+    //do stuff
+});
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+
+}
+
+class Someone {
+    constructor(height, weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+}
+
